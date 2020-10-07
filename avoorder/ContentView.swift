0841413d697a14061b51d61de42d2e3a1483edca
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-10-07 20:01:20 +0200

    Initial Commit

---
+++ avoorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  avoorder
+//
+//  Created by Marvin Messenzehl on 07.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ avoorder/avoorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  avoorderApp.swift
+//  avoorder
+//
+//  Created by Marvin Messenzehl on 07.10.20.
+//
+
+import SwiftUI
+
+@main
+struct avoorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
